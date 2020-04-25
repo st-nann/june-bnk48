f1fac6d3be1958e04e1d790f82c5dd1057dca773
Sanyanee Thawinvongrak <st-nann@users.noreply.github.com>
2020-04-26 00:01:05 +0700

    Update Live 25.04.2020

--- src/services/data/performance/Live.ts
+++ src/services/data/performance/Live.ts
@@ -4,7 +4,7 @@ module.exports = {
       name: '[Live] BNK48 Application',
       description: '',
       time: '',
-      link: 'https://app.bnk48.com/member-playback/125744',
+      link: 'https://app.bnk48.com/member-playback/126060',
       type: 'bnk application'
     }
   ]
